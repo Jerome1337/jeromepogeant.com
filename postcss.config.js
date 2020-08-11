c5873bcf7eaab9d99e3d04ea74ae3a1dd10d8264
Jérôme Pogeant <p-jerome@hotmail.fr>
2020-08-11 18:43:04 +0200

    Add dark/light theme switcher

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,17 @@
 const cssnano = require('cssnano');
-const postcss_color_mod = require('postcss-color-mod-function');
-const postcss_preset_env = require('postcss-preset-env');
-const postcss_Import = require('postcss-import');
-const postcss_Url = require('postcss-url');
+const postcssColorMod = require('postcss-color-mod-function');
+const postcssPresetEnv = require('postcss-preset-env');
+const postcssImport = require('postcss-import');
+const postcssUrl = require('postcss-url');
 const purgecss = require('@fullhuman/postcss-purgecss');
+const tailwindcss = require('tailwindcss');
+const postcssFontMagician = require('postcss-font-magician');
 
 const production = !process.env.ROLLUP_WATCH;
 
 module.exports = {
   plugins: [
-    postcss_Import(),
-    postcss_Url(),
-    require('tailwindcss'),
-    require('postcss-font-magician')({
+    postcssFontMagician({
       variants: {
         'Roboto Mono': {
           300: [],
@@ -20,16 +19,19 @@ module.exports = {
           700: [],
         },
       },
-      foundries: ['google'],
+      foundries: 'google',
       protocol: 'https:',
     }),
-    postcss_preset_env({
+    postcssImport(),
+    postcssUrl(),
+    tailwindcss(),
+    postcssPresetEnv({
       stage: 0,
       autoprefixer: {
         grid: false,
       },
     }),
-    postcss_color_mod(),
+    postcssColorMod(),
     cssnano({
       autoprefixer: false,
       preset: ['default'],
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,33 @@
+const gradients = require('tailwindcss-plugins/gradients');
+
 module.exports = {
   theme: {
-    extend: {
-      fill: () => ({
-        'current': 'currentColor',
-        'gray-600': '#25282c',
-        'gray-800': '#151618',
-      }),
+    colors: {
+      'white': '#ffffff',
+      'brown-light': '#a59886',
+      'brown': '#8e7f69',
+      'yellow': '#d7be90',
+      'yellow-dark': '#ebe0cf',
+      'blue': '#141e30',
+      'blue-dark': '#28416f',
+      'gold': '#d69e2e',
+      'gold-dark': '#975a16',
+      'gray': '#718096',
+      'gray-dark': '#2d3748',
     },
     fontFamily: {
       display: ['Roboto-Mono', 'monospace', 'sans-serif'],
       body: ['Roboto-Mono', 'monospace', 'sans-serif'],
     },
+    gradients: (theme) => ({
+      'gradient-dark': ['-30deg', theme('colors.blue-dark'), theme('colors.blue')],
+      'gradient-light': ['-30deg', theme('colors.yellow-dark'), theme('colors.yellow')],
+    }),
   },
   variants: {
     cursor: ['first'],
+    outline: ['focus'],
+    backgroundColor: ['hover'],
   },
-  plugins: [],
+  plugins: [gradients],
 };
