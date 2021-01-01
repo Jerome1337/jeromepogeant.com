418f10c221bece4f5bf3bb22a1ad84c41c13a4b6
Jérôme Pogeant <p-jerome@hotmail.fr>
2021-01-01 18:36:53 +0100

    fix: main height (#21)

--- src/index.svelte
+++ src/index.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <main
-  class="h-screen flex flex-col justify-between font-body bg-gradient-to-br {$isDarkTheme ? 'from-blue to-blue-dark text-gray' : 'from-yellow to-yellow-dark text-brown'}">
+  class="min-h-screen flex flex-col justify-between font-body bg-gradient-to-br {$isDarkTheme ? 'from-blue to-blue-dark text-gray' : 'from-yellow to-yellow-dark text-brown'}">
   <Header />
   <div class="container m-auto px-6">
     <Presentation />
